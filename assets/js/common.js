b7874075a99d54dc30838b5f12e29a09a54db358
glimja <glimja03@gmail.com>
2023-02-07 16:35:17 +0900

    update all

--- assets/js/common.js
+++ assets/js/common.js
@@ -24,6 +24,7 @@ function onload(){
     __fn_levelOpenBtn();
     __fn_levelCloseBtn();
     __fn_chkboxAll();
+	__fn_chkAllWeek();
 }
 
 // 버튼 토글
@@ -73,5 +74,70 @@ function __fn_chkboxAll(){
 			$("input[name=checkList]:checkbox").prop("checked", false);
 			$(this).siblings("label").removeClass("active");
 		}
+	});
+}
+
+// 전체 요일 선택
+function __fn_chkAllWeek(){
+	$("input[name=chkAllWeek]").click(function(){
+		var checkYn = $("input[name=chkAllWeek]").prop("checked");
+		if(checkYn){
+			$("input[name$=SendYn]").each(function(item, index){
+				$(this).prop("checked", true);
+			});
+		}else{
+			$("input[name$=SendYn]").each(function(item, index){
+				$(this).prop("checked", false);
+			});
+		}
+	});
+}
+
+function setDatepicker(obj) {
+	$.datepicker.setDefaults({
+		dateFormat: 'yy-mm-dd',
+		showMonthAfterYear: true,
+		changeYear: false,
+		changeMonth: false,
+		showOn: "both",
+		buttonText: "날짜선택",
+		yearSuffix: '.',
+		monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+		monthNamesShort: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		/*
+		//주말 비활성
+		beforeShowDay: function(date){
+			var day = date.getDay();
+			return [(day != 0 && day != 6)];
+		},
+		*/
+		//maxDate: 0, //오늘이후날짜 비활성
+		//minDate: 0, //오늘이전날짜 비활성
+	});
+	$('#' + obj + '').datepicker();
+	
+	$('.openDatePicker').datepicker();
+
+	$(".dateNotBefore").datepicker({
+		minDate: 0,
+	});
+	$(".dateNotNext").datepicker({
+		maxDate: 0,
+	});
+
+	//일간
+	$('#searchDt, #searchStartDt, #searchEndDt, #searchDate').datepicker({
+		format: "yyyy-mm-dd",
+		autoclose: true,
+	});
+	//월간
+	$( "#searchYyyyMm" ).datepicker({
+		format: "yyyy-mm",
+		startView: "months", 
+		minViewMode: "months",
+		autoclose: true,
 	});
 }
